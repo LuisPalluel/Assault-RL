8d5f4bdaa78c22e97f368861598d012bdb789552
Luis palluel <palluel.luis@gmail.com>
2020-07-05 23:19:36 +0200

    update

--- Assault.py
+++ Assault.py
@@ -9,6 +9,7 @@ import random
 from Net import DQNet
 from ReplayMemory import ReplayMemory, Transition
 import torch.nn.functional as F
+import PIL.Image as Image
 
 class Assault():
 
@@ -16,26 +17,27 @@ class Assault():
         super(Assault).__init__()
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("Working on " + torch.cuda.get_device_name(self.device))
         self.env = gym.make(env_name)
         self.n_actions = self.env.action_space.n
-        self.eps_start, self.eps_end, self.eps_decay = 0.9, 0.05, 200
-        self.gamma = 0.999
-        self.policy_update = 4
-        self.target_update = 10
+        self.eps_start, self.eps_end, self.eps_decay = 1, 0.02, 50000
+        self.gamma = 0.99
+        self.policy_update = 1
+        self.target_update = 1000
         self.steps_done = 0
 
-        self.resize = Compose([ToPILImage(), Resize(40), ToTensor()])
+        self.resize = Compose([ToPILImage(), Resize(60, interpolation=Image.CUBIC), ToTensor()])
 
         self.policy_net = DQNet(self.get_state(self.env.reset(), 'cpu').unsqueeze(0), self.n_actions).to(self.device)
         self.target_net = DQNet(self.get_state(self.env.reset(), 'cpu').unsqueeze(0), self.n_actions).to(self.device)
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.target_net.eval()
 
-        self.optimizer = torch.optim.RMSprop(self.policy_net.parameters(), lr=0.01)
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=0.0001)
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=0.95)
 
         self.memory = ReplayMemory(10000)
-        self.batch_size = 50
+        self.batch_size = 32
 
         self.env.reset()
 
@@ -45,6 +47,7 @@ class Assault():
         plt.ion()
         plt.show()
 
+        self.losses_hist = []
         total_rewards_hist = []
 
         for ep in range(episodes):
@@ -52,9 +55,10 @@ class Assault():
             next_state = self.get_state(self.env.reset())
 
             total_rewards = 0
+            self.losses = []
 
             for i in count():
-                if ep%20 == 0:
+                if ep%50 == 0:
                     self.env.render()
 
                 state = next_state
@@ -71,18 +75,18 @@ class Assault():
                 if i % self.policy_update == 0:
                     self.optimize_model()
 
-                self.steps_done += 1
-
                 if done:
-                    self.scheduler.step()
+                    #self.scheduler.step()
                     break
 
-            if ep % self.target_update == 0:
-                self.target_net.load_state_dict(self.policy_net.state_dict())
+                if self.steps_done % self.target_update == 0:
+                    self.target_net.load_state_dict(self.policy_net.state_dict())
 
 
             total_rewards_hist.append(total_rewards)
-            plt.plot(total_rewards_hist)
+            self.losses_hist.append(np.mean(self.losses))
+            #plt.plot(total_rewards_hist)
+            plt.plot(self.losses_hist)
             plt.draw()
             plt.pause(0.001)
             print("Episode", ep, ":", total_rewards)
@@ -105,14 +109,26 @@ class Assault():
         action_batch = torch.stack(batch.action)
         reward_batch = torch.cat(batch.reward)
 
+        #print("ONE")
+        #print(state_batch.shape)
+        #print(action_batch)
+        #print(reward_batch)
+        #print(non_final_next_states.shape)
+
         state_actions_values = self.policy_net(state_batch).gather(1, action_batch)
 
         next_state_values = torch.zeros(self.batch_size, device=self.device)
         next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
 
+        #print(next_state_values)
         expected_values = next_state_values * self.gamma + reward_batch
+        #print(expected_values)
 
+        #print("Flag 1")
+        #print(state_actions_values)
+        #print(expected_values.unsqueeze(1))
         loss = F.smooth_l1_loss(state_actions_values, expected_values.unsqueeze(1))
+        self.losses.append(loss.item())
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -123,6 +139,10 @@ class Assault():
     def select_action(self, state):
         sample = random.random()
         eps_threshold = self.eps_end + (self.eps_start-self.eps_end) * math.exp(-1. * self.steps_done / self.eps_decay)
+        self.steps_done += 1
+
+        if self.steps_done % 5000 == 0:
+            print("step:", self.steps_done, "/ eps:", eps_threshold)
 
         if sample > eps_threshold:
             return self.policy_net(state.unsqueeze(0)).max(1)[1]
--- Net.py
+++ Net.py
@@ -7,16 +7,18 @@ class DQNet(nn.Module):
     def __init__(self, input_ex, n_actions):
         super(DQNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
 
         conv_res = self.conv3(self.conv2(self.conv1(input_ex)))
         input_shape = conv_res.view(conv_res.size(0), -1).shape[1]
-        hidden_units = 200
+        hidden_units = 512
+
+        print(conv_res.shape)
 
         self.fc1 = nn.Linear(input_shape, hidden_units)
         self.fc2 = nn.Linear(hidden_units, n_actions)
@@ -25,7 +27,7 @@ class DQNet(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        x = self.fc1(x.view(x.size(0), -1))
+        x = F.relu(self.fc1(x.view(x.size(0), -1)))
         return self.fc2(x)
 
 if __name__ == '__main__':
